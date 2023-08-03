6bd7ce7518e68a80997ded3442f16ebc468f1600
Crowdbotics <engineering@crowdbotics.com>
2023-08-03 03:22:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-haze-dev-106756",
+  url: "https://yellow-haze-dev-106756.botics.co",
+  api: "https://yellow-haze-dev-106756.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
